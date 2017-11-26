bb51782a5a3767989ebb4b1bbd0ae2dcbb712f02
Christian Burk <christianburk@creighton.edu>
2017-11-26 16:48:15 -0600

    Adds file watching [Linting: Watching Files]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css'; // imports css for webpack to bundle
 
 import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
+const courseValue = numeral(Math.random()*1000).format('$0,0.00');
 
 /* [use this in multiline comment to disable a certain rule] */
 
